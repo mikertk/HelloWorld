864f769df07633150b39ea1ccfebb0c16c42234e
tangke <tangke@huoyunren.com>
2016-07-13 15:27:40 +0800

    Initial commit

---
+++ src/com/java/test/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.java.test;
+
+/**
+ * Created by miker on 2016/7/13.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+    }
+}
