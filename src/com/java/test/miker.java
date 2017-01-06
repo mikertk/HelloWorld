85ba8ed9d91d32853826ef37c4fd7090017ffdad
tangke <tangke@huoyunren.com>
2017-01-06 16:36:02 +0800

    增加tangke方法

--- src/com/java/test/miker.java
+++ src/com/java/test/miker.java
@@ -5,8 +5,21 @@ package com.java.test;
  */
 public class miker {
     public static void main(String[] args) {
+        System.out.println("钓鱼岛是中国的！");
+    }
+
+
+
+
+
+}
+
+
+class tangke {
+    public static void main(String[] args) {
         System.out.println("南海是中国的！");
     }
+
 
 
 
